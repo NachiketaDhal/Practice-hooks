7a75ebc1444aed240b4bb7ce70fb8d19cd018cae
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-02-05 23:29:19 +0530

    Add Dropdown component

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current && ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (selected.value === option.value) {
+      return null;
+    }
+
+    return (
+      <div
+        className="item"
+        key={option.value}
+        onClick={() => onSelectedChange(option)} // calls setSelected()
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form" ref={ref}>
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
