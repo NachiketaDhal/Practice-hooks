14c7db01ce9861135f516eea759cd8ba8a1f0f56
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-02-06 22:06:40 +0530

    Add Translate and Convert component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
 import Dropdown from "./Dropdown";
+import Translate from "./Translate";
 
 const items = [
   {
@@ -34,21 +35,19 @@ const options = [
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
       <div className="ui container">
-        <button onClick={() => setShowDropdown(!showDropdown)}>
-          Toggle Dropdown
-        </button>
-        {showDropdown ? (
-          <Dropdown
-            options={options}
-            selected={selected}
-            onSelectedChange={setSelected}
-          />
-        ) : null}
+        {/* (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+        ) */}
+        <Translate />
       </div>
     </div>
   );
